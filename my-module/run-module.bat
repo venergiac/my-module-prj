7e2430a01ec6b204537e13f9fe9eb0a70e97e16a
venergiac <giacomo.veneri@gmail.com>
2017-11-04 11:07:11 +0100

    maven

--- my-module/run-module.bat
+++ my-module/run-module.bat
@@ -1,10 +1,8 @@
-jar -cfe mods/myhello.jar com.my.module.hello.MyHello -C bin .
+jar -cfe mods/myhello.jar com.my.module.hello.MyHello -C target/classes .
 @echo %time%
-java --module-path bin --module com.my.module.hello/com.my.module.hello.MyHello
+java --module-path target/classes;../my-module-api/target/classes;../my-module-it/target/classes  --module com.my.module.hello.test/com.my.module.hello.MyHello
 @echo %time%
-java --module-path mods --module com.my.module.hello/com.my.module.hello.MyHello
+java --module-path target/my-module-0.0.1-SNAPSHOT.jar;../my-module-api/target/my-module-api-0.0.1-SNAPSHOT.jar;../my-module-it//target/my-module-it-0.0.1-SNAPSHOT.jar --module com.my.module.hello.test/com.my.module.hello.MyHello
 @echo %time%
-java --show-module-resolution --limit-modules java.base --module-path mods --module com.my.module.hello/com.my.module.hello.MyHello
-@echo %time%
-java --limit-modules java.base --module-path mods --module com.my.module.hello/com.my.module.hello.MyHello
+java --limit-modules java.base,com.my.module.hello.api,com.my.module.hello.it --module-path target/my-module-0.0.1-SNAPSHOT.jar;../my-module-api/target/my-module-api-0.0.1-SNAPSHOT.jar;../my-module-it//target/my-module-it-0.0.1-SNAPSHOT.jar --module com.my.module.hello.test/com.my.module.hello.MyHello
 @echo %time%
